658e59cc4703df99a726c961af5b355f55160376
Vincent Moulin - Kharmaska <kharmachaitea@gmail.com>
2020-06-03 12:07:22 +0200

    initializing the maze as a text file, creating the Virtual Environment and adding the image assets

---
+++ macgyver_maze.py
@@ -0,0 +1,8 @@
+"""
+In this mini Python game, based on the Pygame library,
+you will have to recover the 3 items inside the maze
+so MacGyver can build an awesome weapon
+to defeat the guardian and escape the maze.
+In case an item would be missing
+before facing the guardian, you lose!
+"""
